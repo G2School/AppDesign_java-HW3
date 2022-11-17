44fab67681c71148f796b6dfb2ef6ab6c0f6d4f6
GinoTu <gino.tu130@gmail.com>
2022-11-17 22:24:57 +0800

    P28

---
+++ HW3/src/hw3/p28.java
@@ -0,0 +1,45 @@
+package hw3;
+
+public class p28
+{
+	public static void main(String[] args)
+	{
+		Carp28 car1;
+		car1 = new Carp28();
+		car1.show();
+		
+		Carp27 car2;
+		car2 = new Carp27(1234, 25.0);
+		car2.show();
+		
+	}
+}
+
+class Carp28
+{
+	private int num;
+	private double gas;
+	
+	public Carp28()
+	{
+		num = 0;
+		gas = 0.0;
+		System.out.println("produce the car");
+	}
+	
+	public Carp28(int n, double g)
+	{
+		this();
+		num = n;
+		gas = g;
+		System.out.println("produce the carwhich car number is " + n + ",volume of gas is " + g);
+	}
+	
+	
+	void show()
+	{
+		System.out.println("Car number is " + this.num);
+		System.out.println("volume of gas is " + this.gas);
+	}
+}
+
