3becbcccc116ae6c9abd7e80f653f3f44e410c0c
GinoTu <gino.tu130@gmail.com>
2022-11-17 22:52:52 +0800

    P36

---
+++ HW3/src/hw3/p36.java
@@ -0,0 +1,18 @@
+package hw3;
+
+public class p36 
+{
+	public static void main(String[] args)
+	{
+		String str = "Hello";
+		
+		char ch1 = str.charAt(0);
+		char ch2 = str.charAt(1);
+		
+		int len = str.length();
+		
+		System.out.println(str + "first char is " + ch1);
+		System.out.println(str + "second char is " + ch2);
+		System.out.println(str + " length is " + len);
+	}
+}
