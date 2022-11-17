4da0f766a7c171066a20563ce01559754d9f0ca9
GinoTu <gino.tu130@gmail.com>
2022-11-17 18:42:12 +0800

    P11

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2e/2082516b6466001d1605dda41b49eb35
@@ -0,0 +1,36 @@
+package hw3;
+
+public class p9
+{
+	public static void main(String[] args)
+	{
+		Carp9 car1;
+		car1 = new Carp9();
+		
+		car1.num = 1234;
+		car1.gas = 20.5;
+		
+	}
+}
+
+class Carp9
+{
+	int num;
+	double gas;
+	
+	void show()
+	{
+		System.out.println("Car number is " + this.num);
+		System.out.println("vilume of gas is " + this.gas);
+	}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ HW3/src/hw3/p11.java
@@ -0,0 +1,39 @@
+package hw3;
+
+public class p11
+{
+	public static void main(String[] args)
+	{
+		Carp11 car1;
+		car1 = new Carp11();
+		
+		car1.num = 1234;
+		car1.gas = 20.5;
+		
+		car1.show();
+		car1.show();
+		
+	}
+}
+
+class Carp11
+{
+	int num;
+	double gas;
+	
+	void show()
+	{
+		System.out.println("Car number is " + this.num);
+		System.out.println("vilume of gas is " + this.gas);
+	}
+}
+
+
+
+
+
+
+
+
+
+
