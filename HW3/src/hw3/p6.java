f996b2d7dd60d9f4ec77998949b798b59ab22ad9
GinoTu <gino.tu130@gmail.com>
2022-11-17 16:55:53 +0800

    P6

---
+++ HW3/src/hw3/p6.java
@@ -0,0 +1,31 @@
+package hw3;
+
+public class p6
+{
+	public static void main(String[] args)
+	{
+		Carp6 car1;
+		car1 = new Carp6();
+		
+		car1.num = 1234;
+		car1.gas = 20.5;
+		
+		System.out.println("Car number is " + car1.num);
+		System.out.println("vilume of gas is " + car1.gas);
+	}
+}
+
+class Carp6
+{
+	int num;
+	double gas;
+}
+
+
+
+
+
+
+
+
+
